92bbdf36f48ff63d94363fe0a92aa5c8afecc5d1
cjeecsiy <konny198952+github@gmail.com>
2014-07-06 00:58:28 +0900

    List3.32_mysql(2014/7/6)

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ SampleApp::Application.routes.draw do
   get "static_pages/help"
   get "static_pages/about"
   get "static_pages/contact"
-  # The priority is based upon order of creation: first created -> highest priority.
+# The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
