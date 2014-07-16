9db673b301d217d67a376646bdd2254b3ba67384
cjeecsiy <konny198952+github@gmail.com>
2014-07-16 23:02:07 +0900

    List5.37

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -59,6 +59,6 @@ describe "Static Pages" do
     click_link "Sign up now!"
     expect(page).to have_title(full_title('Sign up'))
     click_link "sample app"
-    #expect(page).to have_title(full_title())
+    expect(page).not_to have_title(full_title('Home'))
   end
 end
