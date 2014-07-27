2eb973dcae93c4422c41f2fcc68715ab71326a89
cjeecsiy <konny198952+github@gmail.com>
2014-07-27 22:46:52 +0900

    List7.11

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # bcrypt'のコスと関数を下げてテスト速度を向上させる
+  ActiveModel::SecurePassword.min_cost = true
 end
