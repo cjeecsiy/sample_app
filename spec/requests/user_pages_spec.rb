cf63bd653a75e65ea8b0de435427e8822c126e04
cjeecsiy <konny198952+github@gmail.com>
2014-08-11 02:34:13 +0900

    Chapter8.5

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -51,7 +51,7 @@ describe "User Pages" do
 
         it { should have_link('Sign out')}
 	it { should have_title(user.name) }
-	it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+	it { should have_welcome_message('Welcome') }
       end
 
       it "should create a user" do
