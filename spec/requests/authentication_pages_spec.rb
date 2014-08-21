8ade0e8815120cbafc9df1b9902d8cacce433ed8
cjeecsiy <konny198952+github@gmail.com>
2014-08-22 00:22:37 +0900

    List9.20

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -63,7 +63,6 @@ describe "Authentication" do
 
 
       describe "in the Users controller" do
-        
 	describe "visiting the edit page" do
           before { visit edit_user_path(user) }
 	  it { should have_title('Sign in') }
@@ -72,6 +71,11 @@ describe "Authentication" do
 	describe "submitting to the update action" do
 	  before { patch user_path(user) }
 	  specify { expect(response).to redirect_to(signin_path) }
+	end
+	
+	describe "visiting the user index" do
+	  before { visit users_path }
+	  it { should have_title('Sign in') }
 	end
       end
     end
