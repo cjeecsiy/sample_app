3675c235117b70ad8ec84b30019c4531522b4ace
cjeecsiy <konny198952+github@gmail.com>
2014-08-24 20:26:34 +0900

    List9.26

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -30,6 +30,7 @@ describe "Authentication" do
       before { sign_in user } 
 
       it { should have_title(user.name) }
+      it { should have_link('Users', href: user_path) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
@@ -37,7 +38,7 @@ describe "Authentication" do
 
       describe "followed by signout" do
         before { click_link "Sign out" }
-	it { should have_link('Sign in') }
+	      it { should have_link('Sign in') }
       end
     end
   end
@@ -48,35 +49,35 @@ describe "Authentication" do
 
       describe "when attempting to visit a protected page" do
         before do
-	  visit edit_user_path(user)
-	  fill_in "Email", with: user.email
-	  fill_in "Password", with: user.password
-	  click_button "Sign in"
-	end
+	        visit edit_user_path(user)
+	        fill_in "Email", with: user.email
+	        fill_in "Password", with: user.password
+	        click_button "Sign in"
+	      end
       
         describe "after signing in" do
           it "should render the desired protected page" do
-	    expect(page).to have_title('Edit user')
-	  end
+	          expect(page).to have_title('Edit user')
+	        end
         end
       end
 
 
       describe "in the Users controller" do
-	describe "visiting the edit page" do
+	      describe "visiting the edit page" do
           before { visit edit_user_path(user) }
-	  it { should have_title('Sign in') }
-	end
+	        it { should have_title('Sign in') }
+	      end
         
-	describe "submitting to the update action" do
-	  before { patch user_path(user) }
-	  specify { expect(response).to redirect_to(signin_path) }
-	end
+	      describe "submitting to the update action" do
+	        before { patch user_path(user) }
+	        specify { expect(response).to redirect_to(signin_path) }
+	      end
 	
-	describe "visiting the user index" do
-	  before { visit users_path }
-	  it { should have_title('Sign in') }
-	end
+	      describe "visiting the user index" do
+	        before { visit users_path }
+	        it { should have_title('Sign in') }
+	      end
       end
     end
     
@@ -87,15 +88,14 @@ describe "Authentication" do
 
       describe "submitting a GET request to the Users#edit action" do
         before { get edit_user_path(wrong_user) }
-	specify { expect(response.body).not_to match(full_title('Edit user')) }
-	specify { expect(response).to redirect_to(root_url) }
+	      specify { expect(response.body).not_to match(full_title('Edit user')) }
+	      specify { expect(response).to redirect_to(root_url) }
       end
 
       describe "submitting a PATCH request to the Users#update action" do
         before { patch user_path(wrong_user) }
-	specify { expect(response).to redirect_to(root_path) }
+	      specify { expect(response).to redirect_to(root_path) }
       end
     end
-	
   end
 end
